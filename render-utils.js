9c427badf0c76925912bfa8eb5f9926823205e6b
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-08-10 14:17:20 -0500

    Update player detail styles

--- render-utils.js
+++ render-utils.js
@@ -24,18 +24,31 @@ export function renderPlayerDetail(player) {
     const div = document.createElement('div');
     const img = document.createElement('img');
     const nameEl = document.createElement('p');
-    const numberEl = document.createElement('p');
+
+    const columnContainerEl = document.createElement('div');
+
+    const leftColumnEl = document.createElement('div');
+    const positionEl = document.createElement('p');
     const ageEl = document.createElement('p');
     const experienceEl = document.createElement('p');
+
+    const rightColumnEl = document.createElement('div');
+    const numberEl = document.createElement('p');
     const heightEl = document.createElement('p');
     const weightEl = document.createElement('p');
+
     const collegeEl = document.createElement('p');
     const bioEl = document.createElement('p');
+
+    div.classList.add('player-details');
 
     img.src = `../assets/${player.first_name}-${player.last_name}.png`;
 
     nameEl.textContent = `${player.first_name} ${player.last_name}`;
+    nameEl.classList.add('player-detail-name');
+
     numberEl.textContent = `#${player.number}`;
+    positionEl.textContent = player.position;
     ageEl.textContent = `Age: ${player.age}`;
     experienceEl.textContent = `Experience: ${player.experience} years`;
     heightEl.textContent = `Height: ${player.height}`;
@@ -43,7 +56,18 @@ export function renderPlayerDetail(player) {
     collegeEl.textContent = `College: ${player.college}`;
     bioEl.textContent = player.bio;
 
-    div.append(img, nameEl, numberEl, ageEl, experienceEl, heightEl, weightEl, collegeEl, bioEl);
+
+    columnContainerEl.classList.add('column-container');
+    leftColumnEl.classList.add('left-column');
+    rightColumnEl.classList.add('right-column');
+
+    bioEl.classList.add('bio');
+
+    leftColumnEl.append(positionEl, ageEl, experienceEl);
+    rightColumnEl.append(numberEl, heightEl, weightEl);
+    columnContainerEl.append(leftColumnEl, rightColumnEl);
+
+    div.append(img, nameEl, columnContainerEl, collegeEl, bioEl);
 
     return div;
 }
