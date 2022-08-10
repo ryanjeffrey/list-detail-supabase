2d165e5e98f7dc997443bdc15c397ca351c16390
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-08-09 19:02:33 -0500

    Create an async function that fetches all items from the roster table in supabase

---
+++ fetch-utils.js
@@ -0,0 +1,15 @@
+const SUPABASE_URL = 'https://nxtckliabtixkexceffw.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im54dGNrbGlhYnRpeGtleGNlZmZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTk2Mzg5MTgsImV4cCI6MTk3NTIxNDkxOH0.cjq0XpKdcTVMbEd38x6tI54kqPmiTVNNM5zI8-p7k5c';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getRoster() {
+    // from the dogs table, select all items
+    const response = await client.from('roster').select('*');
+    // and return the response
+    if (response.error) {
+        throw new Error(response.error.message);
+    }
+    return response.data;
+}
